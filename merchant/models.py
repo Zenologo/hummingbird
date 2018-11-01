b918c5f73c3fee76c48600b899d63f3468f287af
Zenologo <yanleifrance@yahoo.fr>
2018-11-01 10:44:54 +0100

    update ignore file

--- merchant/models.py
+++ merchant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class MyModelName(models.Model):
+    """
+    Merchant's model who representes all infos about merchant.
+    """
+
+    name = models.CharField(max_length=255, help_text="商家名称")
+    web = models.URLField(help_text="商家网站") # Default length: 200
+    tel = models.CharField(max_length=64, help_text="电话")
+    country = models.CharField(max_length=64, help_text="国家")
+    addresse = models.TextField(help_text="商家地址")
+
+    def __str__(self):
+        return self.name
