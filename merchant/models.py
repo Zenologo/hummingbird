cdc5830fa151d44e029d1656d1c86c6809efdbd3
Zenologo <yanleifrance@yahoo.fr>
2019-01-03 17:52:16 +0100

    modif models

--- merchant/models.py
+++ merchant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from product.models import ProductBrand, Product
 
 class Merchant(models.Model):
     '''
@@ -27,7 +28,6 @@ class Merchant(models.Model):
     country = models.CharField(verbose_name= '国家', max_length = 32, 
         choices = COUNTRY_LIST, help_text = '商家所在国家', blank = True)
     addresse = models.TextField(verbose_name = '地址', help_text = '商家地址', blank=True)
-    last_modyfied_time = models.DateTimeField(auto_now_add=True)
 
 
     type = models.CharField(verbose_name = '商家类型', max_length = 5, choices = MERCHANT_TYPE,
@@ -39,32 +39,36 @@ class Merchant(models.Model):
 
 
 class MerchantCatalog(models.Model):
-    '''
+    """
     Website's catalog.
     The catalog is a range of products and a task for Gecko.
-    '''
+    """
+    # name = models.CharField(verbose_name = '品牌名', max_length = 255, help_text='品牌名')
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name = '品牌名', blank = True, max_length = 255, help_text = '品牌名')
+    merchant = models.ForeignKey(Merchant, verbose_name = '商家名称', on_delete = models.CASCADE, null = True, help_text = '商家名称')
+    brand = models.ForeignKey(ProductBrand, on_delete = models.CASCADE, null = True, help_text = '品牌名')
     
-    name = models.CharField(verbose_name = '品牌名', max_length = 255, help_text='品牌名')
     url = models.URLField(verbose_name='品牌链接', help_text='品牌链接', blank=True) # Default length: 200
-    merchant = models.ForeignKey('Merchant', verbose_name = '商家名称', on_delete = models.CASCADE, help_text = '商家名称')
     description = models.TextField(verbose_name = '品牌介绍', help_text='品牌介绍', blank=True)
-    last_modyfied_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return "catalog_" + str(self.id) + "_" + str(self.brand)
 
 class MerchantProduct(models.Model):
     '''
     All merchant's products.
     '''
-
-    name = models.CharField(verbose_name = '产品名', max_length = 255, help_text='产品名')
+    id = models.AutoField(primary_key=True)
+    merchant = models.ForeignKey(Merchant, verbose_name = '商家名称', on_delete = models.CASCADE, null = True, help_text = '商家名称')
+    name = models.CharField(verbose_name = '产品名', blank = True,  max_length = 255, help_text = '产品名')
+    brand = models.ForeignKey(MerchantCatalog, on_delete = models.CASCADE, null = True, help_text = '品牌名')
+    product_name = models.ForeignKey(Product, on_delete = models.CASCADE, null = True, help_text='产品名')
     chinese_name = models.CharField(verbose_name = '中文名称', max_length = 255, help_text='中文名称', blank=True)
     url = models.URLField(verbose_name='目录链接', help_text='产品目录链接', blank=True) # Default length: 200
     price = models.DecimalField(max_digits = 18, verbose_name='价格',  decimal_places=2, help_text='价格', blank=True)
     description = models.TextField(verbose_name = '产品介绍', help_text='产品介绍', blank=True)
     specification = models.CharField(verbose_name = '产品规格', max_length = 16, help_text = '产品规格', blank=True)
-    last_modyfied_time = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.name
+        return "product_" + str(self.id) + "_" + str(self.product_name)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from  merchant.models import Merchant, MerchantCatalog
+# from merchant.models import Merchant, MerchantCatalog
 
 class Product(models.Model):
     """
@@ -27,7 +27,7 @@ class Product(models.Model):
     brand = models.ForeignKey('ProductBrand', verbose_name = '品牌名', on_delete = models.SET_NULL, help_text = '品牌名', null = True)
     description = models.TextField(verbose_name = '产品介绍', help_text='产品介绍', blank=True)
     specification = models.CharField(verbose_name = '产品规格', max_length = 16, help_text = '产品规格', blank=True)
-    created_time = models.DateTimeField(auto_now_add=True)
+    # created_time = models.DateTimeField(auto_now_add=True)
     
     # url = models.URLField(verbose_name='产品链接', help_text='产品链接', blank=True) # Default length: 200
     # price = models.DecimalField(max_digits = 18, verbose_name='价格',  decimal_places=2, help_text='价格', blank=True)
@@ -57,6 +57,7 @@ class ProductBrand(models.Model):
     name = models.CharField(verbose_name = '品牌名', max_length = 255, help_text='品牌名')
     description = models.TextField(verbose_name = '品牌介绍', help_text='品牌介绍', blank=True)
     url = models.URLField(verbose_name='品牌链接', help_text='品牌链接', blank=True) # Default length: 200
+    # created_time = models.DateTimeField(auto_now_add=True)
         
     def __str__(self):
         return self.name
