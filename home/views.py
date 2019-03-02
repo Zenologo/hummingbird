fab1d9552a504b61a572d78aeee1e62b1465fe98
Zenologo <yanleifrance@yahoo.fr>
2019-03-02 23:13:59 +0100

    modif result page

--- home/views.py
+++ home/views.py
@@ -8,18 +8,22 @@ def home_page(request):
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = SearchForm(request.POST)
+        print("home page begin")
         # check whether it's valid:
         if form.is_valid(): 
+
             var_tmp = form.cleaned_data['main_search']
-            lst_product = get_list_or_404(Product, name__contains=var_tmp)
-            return render(request, 'home/resultat.html', {'lst_pro': lst_product})
+            #lst_product = get_list_or_404(Product, name__contains=var_tmp)
+            lst_product = Product.objects.filter(name__contains = var_tmp)
+
+            return render(request, 'home/resultat.html', {'lst_pro': lst_product, 'serach_form' :form})
         else:
             return render(request, 'home/thanks.html')
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SearchForm()
 
-    return render(request, 'home/home.html', {'form': form})
+    return render(request, 'home/home.html', {'serach_form': form})
 
 
 def thanks_page(request):
--- hummingbird/settings.py
+++ hummingbird/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%-=^)pt6os2(mj!ekdx5nx)(p$+*s6t^-3%q*7p)77anlkuw+y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
