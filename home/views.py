fec4524d6874b1799415c3ed380292f17c39438e
Zenologo <yanleifrance@yahoo.fr>
2019-03-24 22:40:59 +0100

    modi reach page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_list_or_404, render
 from .forms import SearchForm
-from product.models import Product 
+#from product.models import Product
+from merchant.models import PriceMonitor, MerchantProduct
 
 def home_page(request):
 
@@ -14,15 +15,42 @@ def home_page(request):
 
             var_tmp = form.cleaned_data['main_search']
             #lst_product = get_list_or_404(Product, name__contains=var_tmp)
-            lst_product = Product.objects.filter(name__contains = var_tmp)
+            # lst_product = Product.objects.filter(name__contains = var_tmp)
 
-            total = 0
-            # Price range : échelle de prix
-            # List product :   name, marque, description, prix_min, prix_max, 
+            lst_product = PriceMonitor.objects.filter(merchant_product__product__name__contains = var_tmp)
 
-            return render(request, 'home/resultat.html', {'lst_product': lst_product, 'total_results': total, 
-                                                         
-            'serach_form' :form})
+            total = len(lst_product)
+            print("Total: " + str(total))
+            
+            set_name = set()
+            lst_items = []
+            for item in lst_product:
+                if item.merchant_product not in set_name:
+                    """ Create a new item in result list"""
+                    set_name.add(item.merchant_product)
+                    item_prod = ItemSearch()
+                    item_prod.name = item.merchant_product.product.name
+                    item_prod.brand = item.merchant_product.brand
+                    item_prod.description = item.description
+
+                    item_prod.price_max = item.price
+                    item_prod.price_min = item.price
+
+                    item_prod.url = "http://127.0.0.1"
+                    lst_items.append(item_prod)
+                else:
+                    for price_item in lst_items:
+                        """ Update price range """
+                        if price_item.name == item.merchant_product:
+                            if price_item.price_max < item.price:
+                                price_item.price_max = item.price
+                            elif price_item.price > item.price:
+                                price_item.price = item.price
+                            break
+                            
+            return render(request, 'home/resultat.html', {'lst_product': lst_items, 
+                                                            'total_results': total,                         
+                                                            'serach_form' :form})
         else:
             return render(request, 'home/thanks.html')
     # if a GET (or any other method) we'll create a blank form
@@ -39,9 +67,9 @@ def resultat_page(request):
     return render(request, 'resulat.html')
 
 
-class ResultResearch:
+class ItemSearch:
     """
-        All results of search. 
+        All results of search page.
     """
     def __init__(self):
         self.price_min = 0
@@ -49,4 +77,5 @@ class ResultResearch:
         self.name = "product name"
         self.description = "product description"
         self.brand = "product's brand"
+        self.url = "http://127.0.0.1"
 
